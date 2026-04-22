1081257bbe6060e0051892091a5c6867a211a305
2439905184 <2439905184@qq.com>
2026-04-22 19:57:13 +0800

    补充无界一键汉化工具

--- index.php
+++ index.php
@@ -459,6 +459,7 @@
                 <li><a href="https://paper2gal.com/">paper2Gal-论文转galgame-好玩的开源项目</a></li>
                 <li><a href="https://reze.studio">reze studio，可以通过webGPU技术在浏览器编辑MMD动画</a></li>
                 <li><a href="https://lainos.org/">LainOS是一个开源项目，旨在通过弥合“外星优雅”与“人类易用性”之间的鸿沟，彻底革新用户体验，从而打造出既符合用户需求又美观大方的全新独特界面。概念文档即将发布。</a></li>
+                <li><a href="https://gto.hs2049.cn/">无界一键汉化工具</a></li>
             </ol>
             <!-- cosplay -->
             <ol class="news-list">
