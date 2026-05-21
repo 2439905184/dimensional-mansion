ea53e76c7aac37055c6322d53d9f6d475442f8c0
2439905184 <2439905184@qq.com>
2026-05-21 08:26:27 +0800

    【为了“最后的体面”，年轻人开始求购二次元骨灰盒-哔哩哔哩】

--- index.php
+++ index.php
@@ -485,6 +485,7 @@
                 <li><a href="https://www.bilibili.com/opus/1182794849037844485">截止到2026-3-23 noesis诉说谎言的记忆的作者（C大）官网已经删除网站内容</a></li>
                 <li><a href="https://game.kasuie.cc/">ACG浓度测试</a></li>
                 <li><a href="http://galgametest.download/">链接 萌新的你，最适合哪款 Galgame？</a></li>
+                <li><a href="https://b23.tv/EaPvADw">【为了“最后的体面”，年轻人开始求购二次元骨灰盒-哔哩哔哩】</a></li>
             </ol>
             <!-- 小说 -->
             <ol class="news-list"></ol>
